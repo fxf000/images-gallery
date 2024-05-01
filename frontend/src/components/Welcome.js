69b1e58a151a1b938fda588942eef8e914859c81
fxf000 <zfxf000fxfz@gmail.com>
2024-05-01 14:37:58 +0900

    add welcome component

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Welcome = () => (
+  <div className="Jumbotron">
+    <h1>Images Gallery</h1>
+    <p>
+      This is simple application that retrive photos using Unsplach API. In
+      order to start enter any search term in the input field.
+    </p>
+    <p>
+      <Button bsStyle="primary" href="https://unsplash.com" targrt="_blank">
+        Learn more
+      </Button>
+    </p>
+  </div>
+);
+
+export default Welcome;
